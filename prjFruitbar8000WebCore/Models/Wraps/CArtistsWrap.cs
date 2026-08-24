cf7063363e990d879812d76fa074d03475c75b56
reboot0707 <bootcamp@contact.clam.tw>
2026-08-24 14:38:34 +0800

    feat: add wrap class for Artists table

---
+++ prjFruitbar8000WebCore/Models/Wraps/CArtistsWrap.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+using prjFruitbar8000WebCore.Models.Entities;
+
+namespace prjFruitbar8000WebCore.Models.Wraps;
+
+public class CArtistsWrap
+{
+    private TArtist _tartist;
+    public TArtist tartist
+    {
+        get { return _tartist; }
+        set { _tartist = value; }
+    }
+
+    public CArtistsWrap()
+    {
+        _tartist = new TArtist();
+    }
+    public CArtistsWrap(TArtist tartist)
+    {
+        _tartist = tartist;
+    }
+
+    [Key]
+    public int FArtistId
+    {
+        get { return _tartist.FArtistId; }
+        set { _tartist.FArtistId = value; }
+    }
+
+    public string FArtistName
+    {
+        get { return _tartist.FArtistName; }
+        set { _tartist.FArtistName = value; }
+    }
+
+    public bool FIsDeleted
+    {
+        get { return _tartist.FIsDeleted; }
+        set { _tartist.FIsDeleted = value; }
+    }
+
+    public string? FArtistType
+    {
+        get { return _tartist.FArtistType; }
+        set { _tartist.FArtistType = value; }
+    }
+}
