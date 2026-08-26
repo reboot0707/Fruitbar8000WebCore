664e953b0bf9b6abbfc24c8849d957f826c56fe1
reboot0707 <bootcamp@contact.clam.tw>
2026-08-26 14:30:13 +0800

    feat: 建立「歌曲建檔」介面
    透過此介面建檔之歌曲預計一併完成對專輯與創作者之關聯

---
+++ prjFruitbar8000WebCore/Models/ViewModels/NewSongViewModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace prjFruitbar8000WebCore.Models.ViewModels;
+
+public class NewSongViewModel
+{
+    [DisplayName("樂曲名稱")]
+    public string? SongName { get; set; }
+    [DisplayName("樂曲創作者")]
+    public int? ArtistId { get; set; }
+    [DisplayName("收錄專輯")]
+    public int? AlbumId { get; set; }
+
+    // NEXT-TODO: 使用新式 C# 語法糖初始化 ( ` = [ ]; ` )
+    public List<SelectListItem>? ArtistList { get; set; }
+
+    // NEXT-TODO: 使用新式 C# 語法糖初始化 ( ` = [ ]; ` )
+    public List<SelectListItem>? AlbumList { get; set; }
+}
