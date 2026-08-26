30ddb46d92f6523d6635932e56f8d2dcac97e76b
reboot0707 <bootcamp@contact.clam.tw>
2026-08-26 16:55:20 +0800

    feat: 實作「歌曲建檔」資料庫寫入核心邏輯

--- prjFruitbar8000WebCore/Models/ViewModels/NewSongViewModel.cs
+++ prjFruitbar8000WebCore/Models/ViewModels/NewSongViewModel.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace prjFruitbar8000WebCore.Models.ViewModels;
@@ -7,8 +8,12 @@ public class NewSongViewModel
 {
     [DisplayName("樂曲名稱")]
     public string? SongName { get; set; }
+
+    [Required]
     [DisplayName("樂曲創作者")]
     public int? ArtistId { get; set; }
+
+    [Required]
     [DisplayName("收錄專輯")]
     public int? AlbumId { get; set; }
 
