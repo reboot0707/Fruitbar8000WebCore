4cac18f84d93992741c4cdb5bd02c8f0e3d516c2
reboot0707 <bootcamp@contact.clam.tw>
2026-08-28 01:09:36 +0800

    fix/chore(ui): 修正欄位驗證提示訊息無法顯示之錯誤, 優化相關欄位中文顯示內容

--- prjFruitbar8000WebCore/Models/ViewModels/GallerySongViewModel.cs
+++ prjFruitbar8000WebCore/Models/ViewModels/GallerySongViewModel.cs
@@ -9,15 +9,15 @@ public class GallerySongViewModel
     [ScaffoldColumn(false)]
     public int? id { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "請輸入樂曲名稱")]
     [DisplayName("樂曲名稱")]
     public string? SongName { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "請選擇至少一位創作者")]
     [DisplayName("樂曲創作者 (可用Ctrl/Shift進行多選)")]
     public List<int>? SelectedArtistIdList { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "請選擇至少一張專輯")]
     [DisplayName("收錄專輯 (可用Ctrl/Shift進行多選)")]
     public List<int>? SelectedAlbumIdList { get; set; }
 
--- prjFruitbar8000WebCore/Models/ViewModels/NewSongViewModel.cs
+++ prjFruitbar8000WebCore/Models/ViewModels/NewSongViewModel.cs
@@ -6,14 +6,15 @@ namespace prjFruitbar8000WebCore.Models.ViewModels;
 
 public class NewSongViewModel
 {
+    [Required(ErrorMessage = "請輸入樂曲名稱")]
     [DisplayName("樂曲名稱")]
     public string? SongName { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "請輸入創作者")]
     [DisplayName("樂曲創作者")]
     public int? ArtistId { get; set; }
 
-    [Required]
+    [Required(ErrorMessage = "請輸入收錄專輯")]
     [DisplayName("收錄專輯")]
     public int? AlbumId { get; set; }
 
